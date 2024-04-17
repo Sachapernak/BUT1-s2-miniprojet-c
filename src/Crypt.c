8e61c20d9371fbc21d83af4c29f78990d4126bb2
Sacha Pernak <sacha.pernak@etu.iut-tlse3.fr>
2024-04-17 17:19:26 +0200

    Comit

---
+++ src/Crypt.c
@@ -0,0 +1,211 @@
+/******************************************************************************
+*  ASR => 4R2.04                                                              *
+*******************************************************************************
+*                                                                             *
+*  N° de Sujet : 3                                                            *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Intitulé : Chiffrement de messages                                         *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom-prénom1 :   Oliver DA SILVA                                            *
+*                                                                             *
+*  Nom-prénom2 :   Sacha PERNAK                                               *
+*                                                                             *
+*  Nom-prénom3 :                                                              *
+*                                                                             *
+*  Nom-prénom4 :                                                              *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom du fichier : Crypt.c                                                   *
+*                                                                             *
+******************************************************************************/
+#include "Lecture.h"
+#include <stdio.h>
+#include <string.h>
+
+// Constantes
+#define ALPHABET_SIZE 62 //La taille de l'alphabet: 10 chiffres + 26 maj + 26 minuscules
+#define NB_START '0'     // les chiffres commencent a '0'
+#define NB_END '9'       // Les chiffres finissent a '9'
+#define MAJ_START 'A'    // Les majuscules commencent a 'A'
+#define MAJ_END 'Z'      // Les majuscules finissent a 'Z'
+#define MIN_START 'a'    // Les minuscules commences a 'a'
+#define MIN_END 'z'      // Les minuscules finissent a 'z'
+
+//Declarations de fonctions
+void cesarCrypt(int size, int key, char* text);
+void cesarDecrypt(int size, int key, char* text);
+void cesarCharCrypt(int key, char* ch);
+
+void vigenereCrypt(char* text, int size, char* key, int keySize);
+void vigenereDecrypt(char* text, int size, char* key, int keySize);
+int* vignereIntKey(int keySize, char* key);
+
+
+//Main pour le test
+void main(int argc, char *argv[])
+{
+
+	int arg;
+	int size;
+	char eotxt = '/';
+	int exit;
+
+	int keySize;
+	printf("Veuillez entrer le text \n>");
+	char* text = getText(&size, eotxt, &exit);
+
+	printf("Veuillez entrez la clé \n>");
+	char* key = getOnlyText(&keySize, eotxt);
+
+	printf("\nText d'origine : %s\n",text);
+	vigenereCrypt(text,size,key,keySize-1);
+	printf("\nTexte crypte : %s\n",text);
+	vigenereDecrypt(text,size,key,keySize-1);
+	printf("\nTexte decrypte : %s\n",text);
+
+
+}
+
+
+/**
+ * Chiffre le texte donné en utilisant la technique de chiffrement de César.
+ * Chiffre uniquement l'alphanumérique.
+ * 
+ * @param size La taille du tableau de texte.
+ * @param key La clé de chiffrement.
+ * @param text Un pointeur vers le tableau de texte à chiffrer.
+ * 
+ * @return rien.
+ */
+void cesarCrypt(int size, int key, char* text) {
+	for (int i = 0; i < size-1; i++) {
+		cesarCharCrypt(key, &text[i]);
+	}
+
+}
+
+void cesarDecrypt(int size, int key, char* text){
+	key = ALPHABET_SIZE - key;
+	cesarCrypt(size, key, text);
+}
+
+void vigenereCrypt(char* text, int size, char* key, int keySize){
+	int* keyVal = vignereIntKey(keySize, key);// keysize - 1 si le texte contient '\0'
+	for (int i = 0; i <size; i++){
+		cesarCharCrypt(keyVal[i%keySize], &text[i]);
+	}
+	free(keyVal);
+}
+
+void vigenereDecrypt(char* text, int size, char* key, int keySize){
+	int* keyVal = vignereIntKey(keySize, key);// keysize - 1 si le texte contient '\0'
+	for (int i = 0; i <size; i++){
+		cesarCharCrypt(ALPHABET_SIZE - keyVal[i%keySize], &text[i]);
+	}
+	free(keyVal);
+}
+
+
+
+int* vignereIntKey(int keySize, char* key){
+	int* keyVal = (int*) malloc(keySize * sizeof(int));
+	for(int i = 0; i < keySize; i++ ){
+		if(key[i] >= NB_START && key[i] <= NB_END){
+			keyVal[i] = key[i] - NB_START;
+		} else if (key[i] >= MAJ_START && key[i] <= MAJ_END){
+			keyVal[i] = (key[i] - MAJ_START) + (NB_END - NB_START) + 1;
+		} else if (key[i]>= MIN_START && key[i] <= MIN_END){
+			keyVal[i] = (key[i] - MIN_START) + (NB_END -  NB_START) + (MAJ_END - MAJ_START) + 2;
+		} else { 
+			keyVal[i] = 0;
+		}
+	}
+	return keyVal;
+
+}
+
+/**
+ * Chiffre un caractère donné en utilisant la technique de chiffrement de César.
+ * Chiffre uniquement l'alphanumérique
+ * 
+ * @param key La clé de chiffrement.
+ * @param ch Un pointeur vers le caractère à chiffrer. Le caractère sera modifié pour contenir le caractère chiffré.
+ * 
+ * @return Aucune.
+ */
+void cesarCharCrypt(int key, char* ch){
+	/* 
+     * Pour éviter les problèmes de dépassement de valeurs, le caractère doit être converti en un entier.
+     * Les valeurs de type 'char' deviennent négatives si elles dépassent 127, donc on les traite comme des entiers.
+     */
+	int c = *ch;
+    /*
+     * La difficulté ici est que l'alphanumérique n'est pas continu dans la table ASCII :
+     * - La plage des chiffres '0' à '9' s'étend de 48 à 57.
+     * - La plage des lettres majuscules 'A' à 'Z' s'étend de 65 à 90.
+     * - La plage des lettres minuscules 'a' à 'z' s'étend de 97 à 122.
+     * Nous ne pouvons donc pas simplement utiliser le modulo, car nous risquerions de transformer un caractère 
+     * alphanumérique en un caractère non-alphanumérique !
+     */
+	//La clé de chiffrement ne dépasse pas 62.
+	int keyMod = key % ALPHABET_SIZE; // 62 char : 10 (0-9) + 26 (A-Z) + 26 (a-Z)
+	
+    // Gestion des chiffres
+	if (c >= NB_START && c <= NB_END){
+		c = c+keyMod;
+		// Si le caractère chiffré est un chiffre, on le laisse tel quel
+		if (c <= NB_END) {
+			*ch = c;
+        /* Si le caractère chiffré dépasse '9', nous devons le déplacer dans la plage des lettres majuscules
+         * en calculant la différence entre le caractère et '9', puis en avançant dans la plage des lettres majuscules.
+         */
+		} else if (c-NB_END + MAJ_START-1 <= MAJ_END) {
+			*ch = c-NB_END + MAJ_START-1;
+        /* Si le caractère dépasse la plage des chiffres et des lettres majuscules,
+         * nous le déplaçons dans la plage des lettres minuscules en calculant la différence,
+         * puis en avançant dans la plage des lettres minuscules.
+         */
+		} else if ( c-NB_END + MAJ_START-2 -MAJ_END+MIN_START <= MIN_END){
+			*ch = c-NB_END + MAJ_START-MAJ_END + MIN_START - 2;
+		} else {
+ 		/* Si le caractère dépasse les trois plages, il retourne à sa plage d'origine en tenant compte de la clé de chiffrement. */
+			*ch = (c - keyMod) - (ALPHABET_SIZE - keyMod);
+		}
+
+    // Gestion des lettres majuscules
+	} else if (c >= MAJ_START && c <= MAJ_END){
+		c = c+keyMod;
+		if (c >= MAJ_START && c <= MAJ_END){
+			*ch = c;
+        /* Si le caractère chiffré dépasse la plage des lettres majuscules,
+         * nous le déplaçons dans la plage des lettres minuscules.
+         */
+		} else if (c-MAJ_END + MIN_START-1 >= MIN_START && c-MAJ_END + MIN_START-1 <= MIN_END) {
+			*ch = c-MAJ_END + MIN_START-1;
+
+		} else if (  (c-MAJ_END + MIN_START-2 - MIN_END+NB_START) <= NB_END){
+			*ch = c-MAJ_END + MIN_START-2 - MIN_END+NB_START;
+		} else {
+			*ch = (c-keyMod) - (ALPHABET_SIZE - keyMod);
+		}
+
+    // Gestion des lettres minuscules
+	} else if (c >= MIN_START && c <= MIN_END) {
+		c = c+keyMod;
+		if (c >=MIN_START && c<=MIN_END){
+			*ch = c;
+		} else if (c-MIN_END + NB_START-1 <= NB_END) {
+			*ch = c-MIN_END + NB_START-1;
+		} else if (c-MIN_END + NB_START-2 - NB_END+MAJ_START <= MAJ_END) {
+			*ch = c-MIN_END + NB_START-2 - NB_END+MAJ_START;
+		} else {
+			*ch = (c-keyMod) - (ALPHABET_SIZE - keyMod);
+		}
+	}
+
+}
---
+++ src/Lecture.h
@@ -0,0 +1,122 @@
+/******************************************************************************
+*  ASR => 4R2.04                                                              *
+*******************************************************************************
+*                                                                             *
+*  N° de Sujet : 3                                                            *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Intitulé : Chiffrement de messages                                         *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom-prénom1 :   Oliver DA SILVA                                            *
+*                                                                             *
+*  Nom-prénom2 :   Sacha PERNAK                                               *
+*                                                                             *
+*  Nom-prénom3 :                                                              *
+*                                                                             *
+*  Nom-prénom4 :                                                              *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom du fichier : Lecture.h                                                 *
+*                                                                             *
+******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Declarations des fonctions
+char* getText(int* sSize, char eotxt, int* exitCode);
+char* getOnlyText(int* sSize, char eotxt);
+void clearBuffer();
+
+/**
+ * Cette fonction permet de récuperer du texte ASCII tapé au clavier tant que
+ * caratère de fin de saisie n'est pas encontré. Ce dernier n'est pas inclut dans
+ * le texte. 
+ * 
+ * @param sSize Pointeur d'entier qui enregistre la taille du texte entrée (inclut '\0')
+ * @param eotxt Caractère de fin de saisie
+ * @param exitCode Pointeur d'entier qui enregistre le code de sortie indiquant si une
+ * 		           erreure a été encontré (0 : succès, 1 : erreur). 
+ * 		           Renvoie 1 si le programme encontre un caractère non ASCII.
+ * @return Pointeur vers un string alloué dynamiquement qui contient le texte saisie.
+ * 		   Attention : le pointeur doit etre free() après utilisation !
+ */
+char* getText(int* sSize, char eotxt, int* exitCode) {
+	*exitCode = 0; //met le code d'erreur a 0 par défaut
+	char c;
+	char* txt;
+	int size = 1; //La taille du string alloué dynamiquement
+	txt = (char*) malloc(size * sizeof(char)); //aloue une taille de 1 au string
+	txt[0] = '\0'; //met par défaut le caractère de fin de string
+	do {
+		c = getchar(); //on récup le char
+		if (c != eotxt) { //verifie si le char n'est pas la fin de saisie
+			if ( (c & 128) == 0 ){ //masque binaire, verifie si le char est ASCII
+				size++;
+				//Augmentation de la taille du string de 1
+				txt = (char*)realloc(txt, size * sizeof(char));
+				txt[size-2] = c; // on insère le nouveau char
+				txt[size-1] = '\0'; //on deplace la fin de string
+			} else {
+				/*Si le char n'est pas ascii, on met le code d'erreur a 1
+				  Le char n'est pas ajouté */
+				*exitCode = 1;
+			}
+		}
+	} while (c != eotxt);
+	*sSize = size;
+	clearBuffer();
+	return txt;
+}
+
+/**
+ * Fonction pour vider le buffer d'entrée
+ */
+void clearBuffer() {
+	char c = getchar();
+	//On récupère les bits tant qu'on n'arrive pas au char de fin de ligne
+	while ('\n' != c) {
+		c = getchar();
+	}
+}
+
+/**
+ * Cette fonction permet de récuperer du texte uniquement alpahnumerique, ignore la
+ * ponctuation, les espaces, etc.
+ * 
+ * @param sSize Pointeur d'entier qui enregistre la taille du texte entrée (inclut '\0')
+ * @param eotxt Caractère de fin de saisie
+ * @return Pointeur vers un string alloué dynamiquement qui contient le texte saisie.
+ * 		   Attention : le pointeur doit etre free() après utilisation !
+ */
+
+char* getOnlyText(int* sSize, char eotxt) {
+	char c;
+	char* txt;
+	int size = 1; //La taille du string alloué dynamiquement
+	txt = (char*) malloc(size * sizeof(char)); //aloue une taille de 1 au string
+	txt[0] = '\0'; //met par défaut le caractère de fin de string
+	do {
+		c = getchar(); //on récup le char
+		if (c != eotxt) { //verifie si le char n'est pas la fin de saisie
+			if ( (c >= '0' && c <= '9') || ( c >='A' && c <= 'Z') || (c >='a' && c <='z') ){ 
+				size++;
+				//Augmentation de la taille du string de 1
+				txt = (char*)realloc(txt, size * sizeof(char));
+				txt[size-2] = c; // on insère le nouveau char
+				txt[size-1] = '\0'; //on deplace la fin de string
+			}
+		}
+	} while (c != eotxt);
+	*sSize = size;
+	clearBuffer();
+	return txt;
+}
+
+
+
